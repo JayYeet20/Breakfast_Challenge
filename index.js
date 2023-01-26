fc84136445d5a071bc0b3dbe1a236e0b9729dc6a
Jay <63338983+JayYeet20@users.noreply.github.com>
2023-01-25 19:05:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -7,5 +7,29 @@ table.appendChild(thead);
 table.appendChild(tbody);
 
 // Adding the entire table to the body tag
-document.getElementById('body').appendChild(table);
+    let row_1 = document.createElement('tr');
+    let heading_1 = document.createElement('th');
+    heading_1.innerHTML = "#";
+    let heading_2 = document.createElement('th');
+    heading_2.innerHTML = "Food";
+    heading_2.setAttribute("style","width:1000px");
+    let heading_3 = document.createElement('th');
+    heading_3.innerHTML = "Calories";
+    let heading_4 = document.createElement('th');
+    heading_4.innerHTML = "Carbs";
+    let heading_5 = document.createElement('th');
+    heading_5.innerHTML = "Fats";
+    let heading_6 = document.createElement('th');
+    heading_6.innerHTML = "Protein";
+    row_1.appendChild(heading_1);
+    row_1.appendChild(heading_2);
+    row_1.appendChild(heading_3);
+    row_1.appendChild(heading_4);
+    row_1.appendChild(heading_5);
+    row_1.appendChild(heading_6);
+    thead.appendChild(row_1);
+
+    // Adding the entire table to the body tag
+    document.getElementById('tableFormat').appendChild(thead);
+
 })
